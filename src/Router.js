eeba6401516c5a8a61b59cf4d30e396d8389a1a0
DevAbas <abas.memmedov.94@gmail.com>
2018-10-06 14:54:13 +0400

    Fixed some react-router bugs

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import App from './App';
 import Recipe from './components/Recipe';
 
 const router = () => (
-  <BrowserRouter>
+  <Router>
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/recipe/:id" component={Recipe} />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default router;
